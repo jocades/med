8d8be21953ae3ddc308fd38be5f04cfd575caba6
Jordi Calafat <104111071+jocades@users.noreply.github.com>
2026-06-17 19:37:38 +0200

    add term entity which owns output, init and cleanup, use app as the actual editor state, improve text editing in normal and insert mode

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,15 @@
 use std::io::{self, StdoutLock, Write};
 
-use anyhow::Result;
 use crossterm::event::{KeyEvent, KeyModifiers};
 use crossterm::queue;
 use crossterm::{
     cursor::*,
     event::{self, Event, KeyCode},
-    execute,
     style::*,
     terminal::*,
 };
 
-fn init() -> Result<StdoutLock<'static>> {
-    let mut stdout = io::stdout().lock();
-    execute!(stdout, EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    Ok(stdout)
-}
-
-fn restore(stdout: &mut StdoutLock<'static>) -> Result<()> {
-    execute!(
-        stdout,
-        SetCursorStyle::DefaultUserShape,
-        LeaveAlternateScreen,
-    )?;
-    disable_raw_mode()?;
-    Ok(())
-}
+use med::{init, restore};
 
 #[derive(Debug, Clone, Copy)]
 struct Vec2 {
@@ -61,7 +44,6 @@ impl std::fmt::Display for Mode {
 }
 
 struct App {
-    // stdout: StdoutLock<'static>,
     mode: Mode,
     buffer: Vec<String>,
     cursor: Vec2,
@@ -69,18 +51,6 @@ struct App {
 }
 
 impl App {
-    fn start() -> Self {
-        // let stdout = init().unwrap();
-
-        Self {
-            // stdout,
-            mode: Mode::Normal,
-            buffer: vec![String::new()],
-            cursor: Vec2::zero(),
-            should_quit: false,
-        }
-    }
-
     fn current_line(&self) -> &str {
         &self.buffer[self.cursor.y]
     }
@@ -89,24 +59,46 @@ impl App {
         &mut self.buffer[self.cursor.y]
     }
 
+    fn cursor_max_x(&self) -> usize {
+        let len = self.current_line().len();
+        match self.mode {
+            Mode::Insert => len,
+            Mode::Normal => len.saturating_sub(1),
+        }
+    }
+
+    fn cursor_clamp_x(&mut self) {
+        self.cursor.x = self.cursor.x.min(self.cursor_max_x());
+    }
+
     fn move_left(&mut self) {
         self.cursor.x = self.cursor.x.saturating_sub(1);
     }
 
     fn move_right(&mut self) {
-        if self.cursor.x < self.current_line().len() - (self.mode == Mode::Normal) as usize {
+        if self.cursor.x < self.cursor_max_x() {
             self.cursor.x += 1;
         }
     }
 
     fn move_up(&mut self) {
         self.cursor.y = self.cursor.y.saturating_sub(1);
+        self.cursor_clamp_x();
     }
 
     fn move_down(&mut self) {
         if self.cursor.y < self.buffer.len() - 1 {
             self.cursor.y += 1;
+            self.cursor_clamp_x();
         }
+    }
+
+    fn move_bol(&mut self) {
+        self.cursor.x = 0;
+    }
+
+    fn move_eol(&mut self) {
+        self.cursor.x = self.cursor_max_x();
     }
 }
 
@@ -116,18 +108,32 @@ impl App {
 //     }
 // }
 
-fn render(app: &App, stdout: &mut StdoutLock<'static>) -> Result<()> {
+#[allow(unused)]
+#[derive(Debug, Clone, Copy)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+fn render(app: &App, stdout: &mut StdoutLock<'static>) -> io::Result<()> {
     let (_, h) = size()?;
 
-    queue!(stdout, Clear(ClearType::All), MoveTo(0, 0))?;
+    queue!(stdout, Clear(ClearType::All))?;
 
     // Buffer
     for (i, line) in app.buffer.iter().enumerate() {
-        queue!(stdout, Print(line), MoveTo(0, i as u16 + 1))?;
+        queue!(stdout, MoveTo(0, i as u16), Print(line))?;
     }
 
-    // Bottom bar
-    queue!(stdout, MoveTo(0, h.saturating_sub(1)))?;
+    // Statusline
+    queue!(
+        stdout,
+        MoveTo(0, h.saturating_sub(1)),
+        SetForegroundColor(Color::Black),
+        SetBackgroundColor(Color::White),
+    )?;
     write!(
         stdout,
         "mode: {} row: {} col: {} lines: {}",
@@ -136,6 +142,7 @@ fn render(app: &App, stdout: &mut StdoutLock<'static>) -> Result<()> {
         app.cursor.x,
         app.buffer.len(),
     )?;
+    queue!(stdout, ResetColor)?;
 
     // Cursor
     let cursor_style = match app.mode {
@@ -153,7 +160,7 @@ fn render(app: &App, stdout: &mut StdoutLock<'static>) -> Result<()> {
     Ok(())
 }
 
-fn update(app: &mut App) -> Result<()> {
+fn update(app: &mut App) -> io::Result<()> {
     match event::read()? {
         Event::Key(key) => {
             if key.code == KeyCode::Char('c') && key.modifiers.contains(KeyModifiers::CONTROL) {
@@ -174,17 +181,46 @@ fn update(app: &mut App) -> Result<()> {
 
 fn normal(app: &mut App, key: KeyEvent) {
     match key.code {
+        // normal -> insert
         KeyCode::Char('i') => app.mode = Mode::Insert,
         KeyCode::Char('a') => {
             app.mode = Mode::Insert;
             app.move_right();
         }
+        KeyCode::Char('I') => {
+            app.mode = Mode::Insert;
+            app.cursor.x = 0;
+        }
+        KeyCode::Char('A') => {
+            app.mode = Mode::Insert;
+            app.move_eol();
+        }
+        KeyCode::Char('o') => {
+            app.buffer.insert(app.cursor.y + 1, String::new());
+            app.mode = Mode::Insert;
+            app.cursor.x = 0;
+            app.cursor.y += 1;
+        }
+        KeyCode::Char('O') => {
+            app.buffer.insert(app.cursor.y, String::new());
+            app.mode = Mode::Insert;
+            app.cursor.x = 0;
+        }
 
+        // movement
         KeyCode::Char('h') | KeyCode::Left | KeyCode::Backspace => app.move_left(),
         KeyCode::Char('l') | KeyCode::Right => app.move_right(),
         KeyCode::Char('k') | KeyCode::Up => app.move_up(),
         KeyCode::Char('j') | KeyCode::Down | KeyCode::Enter => app.move_down(),
 
+        KeyCode::Char('$') => {
+            app.move_eol();
+        }
+        KeyCode::Char('0') => {
+            app.move_bol();
+        }
+
+        // edit
         KeyCode::Char('x') => {
             if !app.current_line().is_empty() {
                 app.buffer[app.cursor.y].remove(app.cursor.x);
@@ -194,37 +230,40 @@ fn normal(app: &mut App, key: KeyEvent) {
             }
         }
 
+        KeyCode::Char('D') => {
+            if app.cursor.x < app.current_line().len() {
+                app.buffer[app.cursor.y].truncate(app.cursor.x);
+                app.move_left();
+            }
+        }
+
         _ => {}
     }
 }
 
 fn insert(app: &mut App, key: KeyEvent) {
     match key.code {
+        // insert -> normal
         KeyCode::Esc => {
             app.mode = Mode::Normal;
             app.move_left();
         }
 
+        // movement
         KeyCode::Left => app.move_left(),
         KeyCode::Right => app.move_right(),
         KeyCode::Up => app.move_up(),
         KeyCode::Down => app.move_down(),
 
-        KeyCode::Backspace => {
-            if app.cursor.x > 0 {
-                app.cursor.x -= 1;
-                app.buffer[app.cursor.y].remove(app.cursor.x);
-            } else if app.cursor.y > 0 {
-                // remove line
-                app.buffer.remove(app.cursor.y);
-                app.cursor.y -= 1;
-                app.cursor.x = app.buffer[app.cursor.y].len();
-            }
+        // edit
+        KeyCode::Char(ch) => {
+            app.buffer[app.cursor.y].insert(app.cursor.x, ch);
+            app.cursor.x += 1;
         }
 
         KeyCode::Enter => {
             if app.cursor.x == app.current_line().len() {
-                // at eol: add new empty line
+                // at end: add new empty line
                 app.buffer.insert(app.cursor.y + 1, String::new());
             } else {
                 // at mid: split
@@ -237,39 +276,64 @@ fn insert(app: &mut App, key: KeyEvent) {
             app.cursor.x = 0;
         }
 
-        KeyCode::Char(ch) => {
-            app.buffer[app.cursor.y].insert(app.cursor.x, ch);
-            app.cursor.x += 1;
+        KeyCode::Backspace => {
+            if app.cursor.x > 0 {
+                // at mid or end: remove char
+                app.cursor.x -= 1;
+                app.buffer[app.cursor.y].remove(app.cursor.x);
+            } else if app.cursor.y > 0 {
+                // at not eof and bol: join with upper line
+                let line = app.buffer.remove(app.cursor.y);
+                app.cursor.y -= 1;
+                app.cursor.x = app.current_line().len();
+                app.current_line_mut().push_str(&line);
+            }
         }
 
         _ => {}
     }
 }
 
-fn main() -> Result<()> {
-    let buffer = match std::env::args().nth(1) {
-        Some(path) => std::fs::read_to_string(&path)?
-            .lines()
-            .map(str::to_owned)
-            .collect(),
-        None => vec![String::new()],
-    };
+struct Terminal {
+    stdout: StdoutLock<'static>,
+}
 
-    let mut stdout = init()?;
+impl Terminal {
+    fn new() -> Self {
+        Self { stdout: init() }
+    }
+
+    fn stdout(&mut self) -> &mut StdoutLock<'static> {
+        &mut self.stdout
+    }
+
+    #[allow(unused)]
+    fn draw<R>(&mut self, f: impl FnOnce(&mut StdoutLock<'static>) -> R) -> R {
+        f(&mut self.stdout)
+    }
+}
+
+impl Drop for Terminal {
+    fn drop(&mut self) {
+        restore();
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut term = Terminal::new();
 
     let mut app = App {
         mode: Mode::Normal,
-        buffer,
+        buffer: vec![String::new()],
         cursor: Vec2::zero(),
         should_quit: false,
     };
 
     while !app.should_quit {
-        render(&app, &mut stdout)?;
+        // term.draw(|stdout| render(&app, stdout))?;
+        render(&app, term.stdout())?;
         update(&mut app)?;
     }
-
-    restore(&mut stdout)?;
 
     Ok(())
 }
